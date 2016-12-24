f45855ceba73a3c13831ff34a562aeb69d0c56b2
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2016-12-24 16:29:35 +0900

    commit!!!

--- src/main/java/com/daou/config/EnvConfig.java
+++ src/main/java/com/daou/config/EnvConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -22,6 +23,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @ComponentScan("com.daou")
 @Lazy
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages="com.daou.repository")
 @PropertySources({
 	@PropertySource("classpath:hibernate-basic.properties")
 })
