7a039d026548bc66d393e85e98c18f665b1a5797
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2016-12-27 22:14:57 +0900

    commit!!!

---
+++ src/main/java/com/daou/entity/MemberAdditionalInfomation.java
@@ -0,0 +1,10 @@
+package com.daou.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="member_additional_infomation")
+public class MemberAdditionalInfomation {
+
+}
