c059a568ed8dff7010fb9e169310557aadf8d248
DESKTOP-R310QHC\ryu <kdamduck77@gmail.com>
2018-02-13 23:10:04 +0900

    commit!!!

---
+++ src/main/java/com/hangugi/controller/Index.java
@@ -0,0 +1,12 @@
+package com.hangugi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Index {
+	@RequestMapping(value = "/login")
+	public void index() {
+
+	}
+}
